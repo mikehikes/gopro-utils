c9f8d5821e68cd52e949550250cae5b450553f6c
Michael Handelman <michael@ecotone.local>
2022-04-06 21:10:39 -0400

    first commit

---
+++ homebrew-install.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# from https://github.com/homebrew-ffmpeg/homebrew-ffmpeg
+
+brew tap homebrew-ffmpeg/ffmpeg
+brew install homebrew-ffmpeg/ffmpeg/ffmpeg --with-xvid --with-webp --with-two-lame --with-tesseract --with-srt --with-rtmpdump --with-openssl --with-openh264 --with-libxml2 --with-libvmaf --with-libvidstab --with-libssh --with-librsvg --with-libopenmpt --with-libmodplug
